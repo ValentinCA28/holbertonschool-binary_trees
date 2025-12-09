45a211eeea2558656bd006518671e401d18ce5f6
ValentinCA28 <12286@holbertonstudents.com>
2025-12-09 14:03:38 +0100

    Fix : binary_tree_balance -> insert tree_height / binary_tree_is_full -> insert tree_size

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Size of the tree (number of nodes), or 0 if tree is NULL
+ *
+ * Description: The size of a tree is the total number of nodes it contains.
+ * This includes the root node and all its descendants.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
  * binary_tree_is_full - Checks if a binary tree is full
  * @tree: Pointer to the root node of the tree to check
  *
@@ -17,5 +34,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-		return (binary_tree_size(tree) % 2 == 1);
+	return (binary_tree_size(tree) % 2 == 1);
 }
