39ab5edb9cfc9d577758edb4fd98bde91ef3cce5
ValentinCA28 <12286@holbertonstudents.com>
2025-12-09 14:37:09 +0100

    Implemented binary_tree_uncle (task 18) betty complient

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node, or NULL if node is NULL
+ *         or if node has no uncle (root or child of root)
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandparent;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	grandparent = node->parent->parent;
+
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
+	else
+		return (grandparent->left);
+}
